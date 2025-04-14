4fae021813ed186176da7726266f1bbd85e95efc
mukesh <mukesh@gmail.com>
2025-04-14 17:57:38 +0530

    modief_all

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,6 @@
 pipeline {
     agent any
-
+    
     stages {
         stage('Terraform Init/Plan/Apply') {
             steps {
--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+resource "aws_instance" "server" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "server"
+  }
+}
+
+resource "aws_instance" "client" {
+  ami           = "ami-00bb6a80f01f03502"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "client"
+
+  }
+}
 
